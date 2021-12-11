5bcb562b5c0e0e542ca96068a45315e728746b61
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-11 19:02:24 +0500

    play playlist with python and opencv, jupyter notebook

--- play_youtube-Video/main.py
+++ play_youtube-Video/main.py
@@ -2,26 +2,23 @@ import cv2
 import numpy as np
 import pafy # pip3 install youtube-dl
 
-url = "https://www.youtube.com/watch?v=bjzkmHRLTaI"
-video = pafy.new(url)
-best = video.getbest(preftype="mp4")
+url = "https://youtu.be/p5Z_GGRCI5s"
+# video = pafy.new(url)
+playlist = pafy.get_playlist(url)
+print(playlist)
+# best = video.getbest(preftype="mp4")
+# captura = cv2.VideoCapture() #Youtube
+# captura.open(best.url)
 
-#captura = cv2.VideoCapture('drone.mp4')                 # Video File
-#captura = cv2.VideoCapture(0)                           # Webcam 1,2,3
-#captura = cv2.VideoCapture('http://192.168.1.68:8081/') # IPCamara
-
-captura = cv2.VideoCapture() #Youtube
-captura.open(best.url)
-
-while True:
-    ret, frame = captura.read()
+# while True:
+#     ret, frame = captura.read()
 
 
     
-    cv2.imshow('Salida',frame)
-    k = cv2.waitKey(24)&0xFF
-    if k == 27:
-        break
+#     cv2.imshow('Salida',frame)
+#     k = cv2.waitKey(24)&0xFF
+#     if k == 27:
+#         break
     
-captura.release()
-cv2.destroyAllWindows()
+# captura.release()
+# cv2.destroyAllWindows()
