80c94d790e2ca3d1b3cadb58d332adc8e71834d8
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 08:28:52 +0500

    adding FPS, in Real time Video

--- Edge-Detection/Edges-Detection.py
+++ Edge-Detection/Edges-Detection.py
@@ -1,9 +1,13 @@
 import cv2 as cv 
-
+import time 
 cap = cv.VideoCapture(0)
-
+start_time = time.time()
+frame_counter =0
 while True:
+    frame_counter +=1
     ret, frame = cap.read()
+    fps = frame_counter/(time.time() - start_time)
+    print(fps)
     cv.imshow('img', frame)
     key =cv.waitKey(1)
     if key ==ord('q'):
