59cd302126e282183ef0cee3eec08136a755fe60
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 08:31:31 +0500

    drawing FPS

--- Edge-Detection/Edges-Detection.py
+++ Edge-Detection/Edges-Detection.py
@@ -7,7 +7,8 @@ while True:
     frame_counter +=1
     ret, frame = cap.read()
     fps = frame_counter/(time.time() - start_time)
-    print(fps)
+    # print(fps)
+    cv.putText(frame, f'FPS: {round(fps, 2)}', (40,30), cv.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
     cv.imshow('img', frame)
     key =cv.waitKey(1)
     if key ==ord('q'):
