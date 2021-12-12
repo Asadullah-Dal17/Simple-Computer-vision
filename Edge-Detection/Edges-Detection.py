a26ac0b70f6ab0c5d8d83d2eaa14f6fdf7a16d8f
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 08:38:39 +0500

    finding Edges, with Canny Edge detector

--- Edge-Detection/Edges-Detection.py
+++ Edge-Detection/Edges-Detection.py
@@ -6,9 +6,16 @@ frame_counter =0
 while True:
     frame_counter +=1
     ret, frame = cap.read()
+    # applying Gaussian Blur to Remove the Noise and preserving the Edges 
+    denoise_frame = cv.GaussianBlur(frame, (3,3), 0)
+
+    edges_frame = cv.Canny(denoise_frame, 100,200)
+    cv.imshow('Edges', edges_frame)
     fps = frame_counter/(time.time() - start_time)
     # print(fps)
+
     cv.putText(frame, f'FPS: {round(fps, 2)}', (40,30), cv.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
+    
     cv.imshow('img', frame)
     key =cv.waitKey(1)
     if key ==ord('q'):
