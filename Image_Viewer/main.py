bd4344533bce2733ef1c372c5db3c4c56a131d91
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-13 15:00:12 +0500

    maintain the ratio of image while showing on the screen

--- Image_Viewer/main.py
+++ Image_Viewer/main.py
@@ -9,14 +9,17 @@ for file in files:
     img_path = os.path.join(dir_path, file)
     img =cv.imread(img_path)
     height, width = img.shape[:2]
-    print(700/height)
+    # print(700/height)
+    # maintain the ratio if width is greater then height, then resize based on with, otherwise on based on height. 
     if width>height:
         resize_img=cv.resize(img, None, fx=(700/width), fy=(700/width), interpolation=cv.INTER_AREA)
+    
     elif height>width:
         resize_img=cv.resize(img, None, fx=(700/height), fy=(700/height), interpolation=cv.INTER_AREA)
-    else:resize_img=cv.resize(img, None, fx=(700/width), fy=(700/width), interpolation=cv.INTER_AREA)
-
-        
+    
+    else:
+        resize_img=cv.resize(img, None, fx=(700/width), fy=(700/width), interpolation=cv.INTER_AREA)
+    
     cv.imshow('resize', resize_img)
     cv.waitKey(0)
     # cv.resize()
