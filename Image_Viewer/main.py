b6be7620d3711bbd10e2d4c2bd5446246266b698
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-13 15:42:11 +0500

    creating simple windows with tkinter

--- Image_Viewer/main.py
+++ Image_Viewer/main.py
@@ -1,34 +1,38 @@
 import cv2 as cv 
 import os
+import tkinter as tk
+import PIL.Image, PIL.ImageTk
+
 # loading all the images in directory 
 def load_images(dir_path):
     #  getting the list of file in the dir
     files = os.listdir(dir_path)
     loaded_image = []
+    value = 500
     for file in files:
         # creating complete image path 
         img_path = os.path.join(dir_path, file)
         img =cv.imread(img_path)
         height, width = img.shape[:2]
-        # print(700/height)
+        # print(value/height)
         # maintain the ratio if width is greater then height, then resize based on with, otherwise on based on height. 
         if width>height:
-            resize_img=cv.resize(img, None, fx=(700/width), fy=(700/width), interpolation=cv.INTER_AREA)
+            resize_img=cv.resize(img, None, fx=(value/width), fy=(value/width), interpolation=cv.INTER_AREA)
         elif height>width:
-            resize_img=cv.resize(img, None, fx=(700/height), fy=(700/height), interpolation=cv.INTER_AREA)
+            resize_img=cv.resize(img, None, fx=(value/height), fy=(value/height), interpolation=cv.INTER_AREA)
         else:
-            resize_img=cv.resize(img, None, fx=(700/width), fy=(700/width), interpolation=cv.INTER_AREA)
+            resize_img=cv.resize(img, None, fx=(value/width), fy=(value/width), interpolation=cv.INTER_AREA)
         # cv.imshow('resize', resize_img)
         # cv.waitKey(0)
         loaded_image.append(resize_img)
     print('loaded all files .')
     
     return loaded_image
-# images directory path 
-dir_path ='../images'
-print('loading...')
-images_list =load_images(dir_path)
-    # cv.resize()
-for img in images_list:
-    cv.imshow('img', img)
-    cv.waitKey(0)
+
+# creating GUI 
+window = tk.Tk()
+window.title("Image Viewer")
+
+canvas =tk.Canvas( window, width=720, height=720)
+canvas.pack()
+window.mainloop() 
