d9932cb5adca598a8d504e6315d88786e5cd4b0e
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-14 12:19:27 +0500

    simple image Viewer is complete

--- Image_Viewer/main.py
+++ Image_Viewer/main.py
@@ -9,13 +9,24 @@ def previous(img_list):
     print(counter_1)
     if counter_1 <0:
         counter_1 =0
-    print('previous button pressed 🌹🌹🌹🌹')
+    image = img_list[counter_1]
+    RGB_img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    img_tk = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(RGB_img))
+    panel.config(image=img_tk)
+    panel.image = img_tk
+    print('previous button pressed ')
+
 def Next(img_list):
     global counter_1
     counter_1 +=1
     print(counter_1)
     if counter_1 >len(img_list):
         counter_1 =0
+    image = img_list[counter_1]
+    RGB_img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    img_tk = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(RGB_img))
+    panel.config(image=img_tk)
+    panel.image = img_tk
     # RGB_img = cv.cvtColor(img_list[0], cv.COLOR_BGR2RGB)
     # img_tk = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(RGB_img))
     # root.config(image=img_tk)
@@ -54,13 +65,23 @@ root.title("Image Viewer")
 image =loaded_images[0]
 # img = loaded_images[0]
 # height, width = image.shape[:2]
-
+# creating new windown with in window 
 RGB_img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 img_tk = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(RGB_img))
+#divide window into two sections. One for image. One for buttons
 
-canvas = tk.Canvas(root, width = width, height = height)
-canvas.pack()
-canvas.create_image(50, 50, image=img_tk, anchor=tk.NW)
+top = tk.Frame(root)
+top.pack(side="top")
+bottom = tk.Frame(root)
+bottom.pack(side="bottom")
+panel = tk.Label(root, image = img_tk)
+panel.image = img_tk # keep a reference!
+panel.pack(side = "top", fill = "both", expand = "yes")
+
+
+# canvas = tk.Canvas(root, width = width, height = height)
+# canvas.pack()
+# canvas.create_image(50, 50, image=img_tk, anchor=tk.NW)
 next_btn = tk.Button(root, text="Next", width=6, height=1, command=lambda: Next(loaded_images))
 previous_btn = tk.Button(root, text="Previous", width=6, height=1, command=lambda: previous( loaded_images))
 next_btn.pack()
