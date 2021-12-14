a5187b0cb2580f5e7c8347b0ae31c0cc2da4a5b9
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-14 10:55:05 +0500

    adding logic for image change

--- Image_Viewer/main.py
+++ Image_Viewer/main.py
@@ -2,10 +2,23 @@ import cv2 as cv
 import os
 import tkinter as tk
 import PIL.Image, PIL.ImageTk
-def previous(a):
-    print(a)
-def Next(a):
-    print(a)
+counter_1 =0
+def previous(img_list):
+    global counter_1
+    counter_1 -=1
+    print(counter_1)
+    if counter_1 <0:
+        counter_1 =0
+    print('previous button pressed')
+def Next(img_list):
+    global counter_1
+    counter_1 +=1
+    print(counter_1)
+    if counter_1 >len(img_list):
+        counter_1 =0
+    # RGB_img = cv.cvtColor(img_list[0], cv.COLOR_BGR2RGB)
+    # img_tk = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(RGB_img))
+    # root.config(image=img_tk)
 # loading all the images in directory 
 def load_images(dir_path):
     #  getting the list of file in the dir
@@ -48,8 +61,8 @@ img_tk = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(RGB_img))
 canvas = tk.Canvas(root, width = width, height = height)
 canvas.pack()
 canvas.create_image(50, 50, image=img_tk, anchor=tk.NW)
-next_btn = tk.Button(root, text="Next", width=6, height=1, command=lambda: Next('test'))
-previous_btn = tk.Button(root, text="Previous", width=6, height=1, command=lambda: previous('test'))
+next_btn = tk.Button(root, text="Next", width=6, height=1, command=lambda: Next(loaded_images))
+previous_btn = tk.Button(root, text="Previous", width=6, height=1, command=lambda: previous( loaded_images))
 next_btn.pack()
 previous_btn.pack()
 
