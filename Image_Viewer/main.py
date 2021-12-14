526d6b429ff7dca2e81490a883adebef671d4082
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-14 09:57:09 +0500

    adding Button to GUI

--- Image_Viewer/main.py
+++ Image_Viewer/main.py
@@ -2,7 +2,10 @@ import cv2 as cv
 import os
 import tkinter as tk
 import PIL.Image, PIL.ImageTk
-
+def previous(a):
+    print(a)
+def Next(a):
+    print(a)
 # loading all the images in directory 
 def load_images(dir_path):
     #  getting the list of file in the dir
@@ -38,10 +41,16 @@ root.title("Image Viewer")
 image =loaded_images[0]
 # img = loaded_images[0]
 # height, width = image.shape[:2]
+
 RGB_img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 img_tk = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(RGB_img))
 
 canvas = tk.Canvas(root, width = width, height = height)
 canvas.pack()
 canvas.create_image(50, 50, image=img_tk, anchor=tk.NW)
+next_btn = tk.Button(root, text="Next", width=6, height=1, command=lambda: Next('test'))
+previous_btn = tk.Button(root, text="Previous", width=6, height=1, command=lambda: previous('test'))
+next_btn.pack()
+previous_btn.pack()
+
 root.mainloop() 
