5dd64be8c596d6c9110b3e99b707585b312548b9
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-15 11:51:10 +0500

    QR code detection Complete, Visual data done 👍

--- QR-Code-Detection-Opencv/main.py
+++ QR-Code-Detection-Opencv/main.py
@@ -15,11 +15,15 @@ while True:
     data, bbox, rectified_img = QR_Decoder.detectAndDecode(frame)
     if bbox is not None:
         box =bbox.astype(int)
-        print(box)
+        cv.circle(frame, box[0][0], 5, AiPhile.INDIGO, -1)
         frame=AiPhile.fillPolyTrans(frame, box, AiPhile.GREEN, 0.5)
+        AiPhile.textBGoutline(frame, f"data: {data}",position=  box[0][0],fonts=cv.FONT_HERSHEY_PLAIN, scaling=0.9)
+
+        # cv.waitKey(0)
     fps = frame_counter/(time.time() - start_time)
 
     AiPhile.textBGoutline(frame, f"FPS: {round(fps,2)}", fonts=cv.FONT_HERSHEY_PLAIN, scaling=1.3)
+       
     cv.imshow('frame', frame)
     key =cv.waitKey(1)
     if key==ord('q'):
