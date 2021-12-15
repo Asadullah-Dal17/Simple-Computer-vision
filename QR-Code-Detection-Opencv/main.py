680ead19a3387fedb7d254de4cd86643a826d8df
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-15 11:23:29 +0500

    QR code detection complete

--- QR-Code-Detection-Opencv/main.py
+++ QR-Code-Detection-Opencv/main.py
@@ -6,13 +6,19 @@ cap = cv.VideoCapture(0)
 
 frame_counter =0
 start_time = time.time()
-
+QR_Decoder = cv.QRCodeDetector()
 while True:
     frame_counter +=1
     ret, frame = cap.read()
     if ret is None:
         break
+    data, bbox, rectified_img = QR_Decoder.detectAndDecode(frame)
+    if bbox is not None:
+        box =bbox.astype(int)
+        print(box)
+        frame=AiPhile.fillPolyTrans(frame, box, AiPhile.GREEN, 0.5)
     fps = frame_counter/(time.time() - start_time)
+
     AiPhile.textBGoutline(frame, f"FPS: {round(fps,2)}", fonts=cv.FONT_HERSHEY_PLAIN, scaling=1.3)
     cv.imshow('frame', frame)
     key =cv.waitKey(1)
