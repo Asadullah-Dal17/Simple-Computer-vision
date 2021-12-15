4f610fde3436d6310d30e0ca562bd04f917f3ef2
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-15 11:01:12 +0500

    adding FPS counter

--- QR-Code-Detection-Opencv/main.py
+++ QR-Code-Detection-Opencv/main.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import time 
+import AiPhile
 cap = cv.VideoCapture(0)
 
 frame_counter =0
@@ -9,6 +10,10 @@ start_time = time.time()
 while True:
     frame_counter +=1
     ret, frame = cap.read()
+    if ret is None:
+        break
+    fps = frame_counter/(time.time() - start_time)
+    AiPhile.textBGoutline(frame, f"FPS: {round(fps,2)}", fonts=cv.FONT_HERSHEY_PLAIN, scaling=1.3)
     cv.imshow('frame', frame)
     key =cv.waitKey(1)
     if key==ord('q'):
