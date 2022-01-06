15e6adedb17b3c49de5f5200879e166ae8506d1a
asadullah-dal-17 <asadullah.dal17@gmail.com>
2022-01-06 18:36:01 +0500

    masking thing out

--- overalayPNG/main.py
+++ overalayPNG/main.py
@@ -3,9 +3,22 @@ import numpy as np
 
 fourground = cv.imread('cheeta.png', cv.IMREAD_UNCHANGED)
 back_ground = cv.imread('pexels-francesco-ungaro-2835436.jpg')
-
+cv.imshow('img', back_ground)
+fg_h, fg_w = fourground.shape[:2]
+x, y = 20, 20
+roi = back_ground[y:fg_h+y, x: fg_w+x ]
+cv.imshow('roi', roi)
 # gettting images channels 
 B, G, R, AlPha = cv.split(fourground)
 cv.imshow("Alpha", AlPha)
+inv_mask = cv.bitwise_not(AlPha)
+cv.imshow('inv', inv_mask)
+out=cv.bitwise_and(roi, roi,mask=inv_mask)
+out2=cv.bitwise_or(fourground,fourground, out,mask=AlPha)
+
+cv.imshow('roi2', out)
+cv.imshow('roi3', out2)
+
+
 cv.imshow('fourground', fourground)
 cv.waitKey(0)
