fb2c681e52453ae40a18d16d00c3bddf5dbd34a6
Asadullah-Dal17 <asadullah.dal17@gmail.com>
2022-01-31 14:04:24 +0500

    updating file

--- Digital_clock/main.py
+++ Digital_clock/main.py
@@ -3,11 +3,21 @@ import cv2 as cv
 from datetime import datetime
 
 while True:
-    img = np.zeros((500, 500, 3), dtype=np.uint8)
+    img = np.zeros((100, 210, 3), dtype=np.uint8)
 
     now = datetime.now()
-    current_time = now.strftime("%H %M %S")
+    current_time = now.strftime("%H:%M:%S")
     print(current_time)
+    cv.putText(
+        img=img,
+        text=f"Time: {current_time}",
+        org=(10, 50),
+        fontFace=cv.FONT_HERSHEY_PLAIN,
+        fontScale=1.5,
+        color=(0, 255, 0),
+        lineType=cv.LINE_AA,
+        thickness=2,
+    )
 
     cv.imshow("frame", img)
 
