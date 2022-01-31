76075f76e8969396fdf788f0f5ab21756d8facbf
Asadullah-Dal17 <asadullah.dal17@gmail.com>
2022-01-31 13:58:48 +0500

    getting the current time

--- Digital_clock/main.py
+++ Digital_clock/main.py
@@ -1,8 +1,13 @@
 import numpy as np
 import cv2 as cv
+from datetime import datetime
 
 while True:
     img = np.zeros((500, 500, 3), dtype=np.uint8)
+
+    now = datetime.now()
+    current_time = now.strftime("%H %M %S")
+    print(current_time)
 
     cv.imshow("frame", img)
 
